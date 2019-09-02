3af510283ecb6dd9c6c76d81d32f16fe36288f17
ShneorUshki <shneoru770@gmail.com>
2019-09-02 23:25:37 +0300

    Creating an API for POST Requests and Unique mail check
    I created an API that accepts POST requests, and adds Mongo DB documents, if there is an email address already in DB will return an error and it will not be added to DB. I have compiled the Mongoose Schema to validate data with a validator directory.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/codeTest', {
+mongoose.connect('mongodb://127.0.0.1:27017/codeTest', {
 	autoReconnect: true,
 	reconnectTries: 60,
 	reconnectInterval: 10000
@@ -11,8 +11,37 @@ const app = express();
 app.listen(3000);
 
 app.use(require('body-parser').json());
+
 app.use('/account/create', require('./api/account/create'));
 
 console.log('app running on port 3000...');
 
 module.exports = app;
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+ 
+	const Account = require('./models/account/Account.js');
+	
+	  app.post('/adduser', function (req, res) {
+		let {email,name,age} = req.body
+
+		let user = new Account(
+			{email,name,age})
+
+			user.save(function(err) {
+				if (err) {
+				  if (err.name === 'MongoError' && err.code === 11000) {
+					
+					return res.status(422).send(
+						{"error": "email already exists"});
+					 }}
+			
+				res.json({
+				  success: true
+				});
+			
+			  });
+			})
+});
--- models/account/Account.js
+++ models/account/Account.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose');
+let validator = require('validator')
 
 const Account = new mongoose.Schema({
-	email: {type: String, required: true},
+	email: {
+		type: String,
+		required: true,
+		lowercase: true,
+		unique: true,
+		validate: (value) => {
+			return validator.isEmail(value)
+		  }
+		},
 	name: {type: String},
 	age: {type: Number},
 
