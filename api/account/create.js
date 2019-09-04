10c43fe5131a1e125d621bcdb14b9d4596d2342d
ShneorUshki <shneoru770@gmail.com>
2019-09-04 17:07:08 +0300

    Enhance interface receive only POST requests
    I added a condition that checks whether the method is different in content and type from POST and if it is different will return an error.

--- api/account/create.js
+++ api/account/create.js
@@ -1,6 +1,9 @@
 const Account = require('../../models/account/Account');
 
 module.exports = async function(req, res, next) {
+	if (req.method !== "POST") {
+		return res.send({"error": "Only POST requests are possible!"})
+	}
 	const {email, name, age} = req.body;
 	const account = new Account({email, name, age});
 	await account.save();
