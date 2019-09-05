3f5ff3ec02535d9274a959ab528637379b2d3952
ShneorUshki <shneoru770@gmail.com>
2019-09-05 12:36:02 +0300

    I added a GET request for accountId
    You can get a user by requesting / notifications /? AccountId = {id}
    The parameter is passed as query param

--- api/notifications/createNotif.js
+++ api/notifications/createNotif.js
@@ -4,15 +4,24 @@ const router = express.Router();
 
 router.post("/add-notifications", async (req, res) => {
   const { accountId, name, color } = req.body;
-  console.log('accountId:', accountId)
   const notifications = new Notifications({ accountId, name, color });
-  await notifications.save().then(() => {
-	 res.send({ message: "success" });
-    } 
-  )
-  .catch((err) =>{
-	res.send({"error": err.message});
-  })
-  
+  await notifications
+    .save()
+    .then(() => {
+      res.send({ message: "success" });
+    })
+    .catch(err => {
+      res.send({ error: err.message });
+    });
+});
+
+router.get("/", async (req, res) => {
+  const user = await Notifications.findOne(req.query)
+    .then(user => {
+      res.send({ user });
+    })
+    .catch(err => {
+      res.send({ error: err.message });
+    });
 });
 module.exports = router;
